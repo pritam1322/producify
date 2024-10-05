1bac75496741327709a0de3c467d84cd3b4a36c5
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2024-10-05 22:09:57 +0530

    Update LogoutButton.tsx

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -3,11 +3,6 @@ import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {signOut} from "next-auth/react";
 
-interface LogoutButtonProps {
-    className?: string;
-    iconLeft?: boolean;
-    iconClasses?: string;
-}
 
 export default function LogoutButton({
     className = "flex items-center gap-6 border p-1 px-4 shadow",
