8217fce28a2731a795b6453465533759b9d93eae
pritam1322 <pritamjat98@gmail.com>
2024-10-06 10:24:27 +0530

    updated auth/api/[...nextauth].route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,9 @@
 
 
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/lib/authOptions";
+import NextAuth from "next-auth";
 
-const prisma = new PrismaClient();
 
-export const authOptions : NextAuthOptions = {
-    secret : process.env.SECRET,
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-          })
-    ]
-};
 
 const handler = NextAuth(authOptions)
 
---
+++ src/lib/authOptions.ts
@@ -0,0 +1,17 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions : NextAuthOptions = {
+    secret : process.env.SECRET,
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+          })
+    ]
+};
